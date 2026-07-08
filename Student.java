0b590a2a6747540d44da651f552c9d2abee0b87a
gautham-afk <gautham@archimedis.io>
2026-07-08 16:34:09 +0530

    streams , Comparable and Comparator
    s

--- Student.java
+++ Student.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -9,13 +10,15 @@ class Students {
     String name;
     private final int marks;
     private final int rollNo;
+    List<Integer> rollnoo;
     List<String> names;
 
-    Students(String name, int marks, int rollNo, List<String> names) {
+    Students(String name, int marks, int rollNo, List<String> names, List<Integer> rollnoo) {
         this.name = name;
         this.marks = marks;
         this.rollNo = rollNo;
         this.names = names;
+        this.rollnoo = rollnoo;
     }
 
     void display() {
@@ -59,6 +62,56 @@ class Students {
 
         System.out.println(numbers);
     }
+
+    void streamnamefilter() {
+        names.stream()
+                .filter(s -> s.startsWith("A"))
+                .limit(2)
+                .map(String::toUpperCase)
+                .forEach(System.out::println);
+    }
+
+    void streamnamefilter2() {
+        names.stream()
+                .sorted()
+                .distinct()
+                .skip(2)
+                .forEach(System.out::println);
+    }
+
+    void streamnamefilter3() {
+        names.stream()
+                .forEach(System.out::println);
+    
+        names.parallelStream()
+                .forEach(System.out::println);
+    }
+
+    void  comporqator() {
+        names.sort((s1, s2) -> s1.compareTo(s2));
+        System.out.println(names);
+
+        
+    }
+
+    void compirtordesc(){
+        Comparator<String> byNameDesc = (s1, s2) -> s2.compareTo(s1);
+        names.sort(byNameDesc);
+        System.out.println(names);    
+        }
+
+    void comporator_rolno() {
+
+        rollnoo.sort((r1, r2) -> r1.compareTo(r2));
+        System.out.println(rollnoo);
+    }
+
+    void comporator_rolno_desc() {
+
+    rollnoo.sort(Comparator.reverseOrder());
+
+    System.out.println(rollnoo);
+}
 }
 
 public class Student {
@@ -69,10 +122,27 @@ public class Student {
                 "John",
                 "Alice",
                 "Bob",
-                "David"
+                "David",
+                "Eve",
+                "Frank",
+                "Grace",
+                "Hannah",
+                "Ivy"
         );
 
-        Students s1 = new Students("Gautham", 39, 1, names);
+        List<Integer> rollnoo = Arrays.asList(
+                21,
+                22,
+                23,
+                24,
+                25,
+                26,
+                27,
+                28,
+                29
+        );
+
+        Students s1 = new Students("Gautham", 39, 1, names, rollnoo);
 
         s1.display();
         s1.isPassed();
@@ -80,5 +150,12 @@ public class Student {
         s1.displayNamesWithLambda();
 
         s1.sortNumbers();
+        s1.streamnamefilter();
+        s1.streamnamefilter2();
+        s1.streamnamefilter3();
+        s1.comporqator();
+        s1.compirtordesc();
+        s1.comporator_rolno();
+        s1.comporator_rolno_desc();
     }
 }
